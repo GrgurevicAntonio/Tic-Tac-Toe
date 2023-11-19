eedbeb78fbe2751a0929882ad4b37dd8b0d5dbe7
AntonioGrgurevic <antoniogrgurevic1802@gmail.com>
2023-11-19 18:58:15 +0100

    Bol

--- main.py
+++ main.py
@@ -1,119 +1,121 @@
 from tkinter import *
-
-def button_press(num):
-    global equation_text
-
-    equation_text = equation_text + str(num)
-    equation_label.set(equation_text)
-
-def equals():
-    global equation_text
-
-    try:
-        total = str(eval(equation_text))
-
-        equation_label.set(total)
-
-        equation_text = total
-    except ZeroDivisionError:
-        equation_label.set("arithmetic error")
-        equation_text = ""
-
-    except SyntaxError:
-        equation_label.set("syntax error")
-        equation_text = ""
+import random
 
 
-def clear():
+def next_turn(row, column):
 
-    global equation_text
+    global player
 
-    equation_label.set("")
+    if buttons[row][column]["text"] == "" and check_winner() is False:
+        if player == players[0]:
+            buttons[row][column]["text"] = player
+            if check_winner() is False:
+                player = players[1]
+                label.config(text=(players[1] + " turn"))
 
-    equation_text = ""
+            elif check_winner() is True:
+                label.config(text=(players[0] + " wins"))
+            elif check_winner() == "Tie":
+                label.config(text="Tie")
+        else:
+            buttons[row][column]["text"] = player
+            if check_winner() is False:
+                player = players[0]
+                label.config(text=(players[0] + " turn"))
+
+            elif check_winner() is True:
+                label.config(text=(players[1] + " wins"))
+            elif check_winner() == "Tie":
+                label.config(text="Tie")
+
+
+
+def check_winner():
+
+    for row in range(3):
+        if buttons[row][0]["text"] == buttons[row][1]["text"] == buttons[row][2]["text"] != "":
+            buttons[row][0].config(bg="green")
+            buttons[row][1].config(bg="green")
+            buttons[row][2].config(bg="green")
+            return True
+    for column in range(3):
+        if buttons[0][column]["text"] == buttons[1][column]["text"] == buttons[2][column]["text"] != "":
+            buttons[0][column].config(bg="green")
+            buttons[1][column].config(bg="green")
+            buttons[2][column].config(bg="green")
+            return True
+
+    if buttons[0][0]["text"] == buttons[1][1]["text"] == buttons[2][2]["text"] != "":
+        buttons[0][0].config(bg="green")
+        buttons[1][1].config(bg="green")
+        buttons[2][2].config(bg="green")
+        return True
+
+    elif buttons[0][2]["text"] == buttons[1][1]["text"] == buttons[2][0]["text"] != "":
+        buttons[0][2].config(bg="green")
+        buttons[1][1].config(bg="green")
+        buttons[2][0].config(bg="green")
+        return True
+    elif empty_spaces() is False:
+
+        for row in range(3):
+            for column in range(3):
+                buttons[row][column].config(bg="red")
+        return "Tie"
+
+    else:
+        return False
+
+
+def empty_spaces():
+
+     spaces = 9
+     for row in range(3):
+         for column in range(3):
+             if buttons[row][column]["text"] != "":
+                 spaces -= 1
+     if spaces == 0:
+       return False
+     else:
+        return True
+
+def new_game():
+
+    global player
+
+    player = random.choice(players)
+
+    label.config(text=player + " turn")
+
+    for row in range(3):
+        for column in range(3):
+            buttons[row][column].config(text="", bg="#F0F0F0")
 
 
 window = Tk()
+window.title("Tic Tac Toe")
+players = ["x", "o"]
 
-window.title("Calculator")
-window.geometry("500x500")
+player = random.choice(players)
+buttons = [[0,0,0],
+        [0,0,0],
+        [0,0,0]]
 
-equation_text= ""
-equation_label = StringVar()
+label = Label(text=player + " turn", font=("consolas", 40))
+label.pack(side="top")
 
-label = Label(window, textvariable=equation_label,
-              font=("consolas", 20,), bg="white", width=24, height=2)
-label.pack()
+
+reset_btn = Button(text="Reset", font=("consolas", 20), command=new_game)
+reset_btn.pack(side="top")
 
 frame = Frame(window)
 frame.pack()
 
-button1 = Button(frame, text= 1, height=4, width=9, font=35,
-                 command= lambda: button_press(1))
-button1.grid(row=0, column=0)
-
-button2 = Button(frame, text=2, height=4, width=9, font=35,
-                 command= lambda: button_press(2))
-button2.grid(row=0, column=1)
-
-button3 = Button(frame, text=3 , height=4, width=9, font=35,
-                 command= lambda: button_press(3))
-button3.grid(row=0, column=2)
-
-button4 = Button(frame, text=4 , height=4, width=9, font=35,
-                 command= lambda: button_press(4))
-button4.grid(row=1, column=0)
-
-button5 = Button(frame, text=5, height=4, width=9, font=35,
-                 command= lambda: button_press(5))
-button5.grid(row=1, column=1)
-
-button6 = Button(frame, text=6, height=4, width=9, font=35,
-                 command= lambda: button_press(6))
-button6.grid(row=1, column=2)
-
-button7 = Button(frame, text=7, height=4, width=9, font=35,
-                 command= lambda: button_press(7))
-button7.grid(row=2, column=0)
-
-button8 = Button(frame, text=8, height=4, width=9, font=35,
-                 command= lambda: button_press(8))
-button8.grid(row=2, column=1)
-
-button9 = Button(frame, text=9, height=4, width=9, font=35,
-                 command= lambda: button_press(9))
-button9.grid(row=2, column=2)
-
-button0 = Button(frame, text=0, height=4, width=9, font=35,
-                 command= lambda: button_press(0))
-button0.grid(row=3, column=0)
-
-plus = Button(frame, text="+", height=4, width=9, font=35,
-                 command= lambda: button_press("+"))
-plus.grid(row=0, column=3)
-
-minus = Button(frame, text="-", height=4, width=9, font=35,
-                 command= lambda: button_press("-"))
-minus.grid(row=1, column=3)
-
-multiply = Button(frame, text="*", height=4, width=9, font=35,
-                 command= lambda: button_press("*"))
-multiply.grid(row=2, column=3)
-
-divide = Button(frame, text="/", height=4, width=9, font=35,
-                 command= lambda: button_press("/"))
-divide.grid(row=3, column=3)
-
-equal = Button(frame, text="=", height=4, width=9, font=35,
-                 command=equals)
-equal.grid(row=3, column=2)
-
-decimal = Button(frame, text=".", height=4, width=9, font=35,
-                 command= lambda: button_press("."))
-decimal.grid(row=3, column=1)
-
-clear = Button(window, text="Clear", height=4, width=12, font=35,
-                 command= clear)
-clear.pack()
-
+for row in range(3):
+    for column in range(3):
+        buttons[row][column] = Button(frame, text="",
+                                      font=("consolas",40),
+                                      width=5, height=2,
+                                      command=lambda row=row, column=column: next_turn(row, column)) #ak ne bude valjalo ovdje moras row i column
+        buttons[row][column].grid(row=row, column=column)
 window.mainloop()
